8cae90fcaedd8b3903ab3579dcce1798084234fa
yifei <364398311@qq.com>
2024-08-30 22:47:57 +0800

    chore(FileStorageController.java): 🔨 由于接口未进行权限控制，该接口只做本地测试

--- yf-boot-admin/yf-admin/src/main/java/com/yf/controller/file/FileStorageController.java
+++ yf-boot-admin/yf-admin/src/main/java/com/yf/controller/file/FileStorageController.java
@@ -1,15 +1,11 @@
 package com.yf.controller.file;
 
-import com.yf.constraints.MultipartFileValid;
-import com.yf.model.result.Result;
 import com.yf.storage.FileStorageService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.Pattern;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 文件上传服务
@@ -17,40 +13,40 @@ import org.springframework.web.multipart.MultipartFile;
  * @author yiFei
  * @since 2023/12/2 13:37
  */
+@Validated
 @RestController
 @RequestMapping("file")
 @Tag(name = "文件上传服务")
-@Validated
+@RequiredArgsConstructor
 public class FileStorageController {
 
-    @Autowired
-    private FileStorageService fileStorageService;
+    private final FileStorageService fileStorageService;
 
-    @PostMapping("/upload")
-    @Operation(summary = "上传文件")
-    public Result<String> uploadFile(
-            @RequestParam(required = false, defaultValue = "")
-            @Pattern(regexp = "^(?!/).*$", message = "存储路径不能以'/'开头") String savePath,
-            @RequestParam @MultipartFileValid MultipartFile file) {
-        return Result.success(fileStorageService.uploadFile(savePath, file));
-    }
-
-    @PostMapping("/uploads")
-    @Operation(summary = "上传多个文件")
-    public Result<String[]> uploadFiles(
-            @RequestParam(required = false, defaultValue = "")
-            @Pattern(regexp = "^(?!/).*$", message = "存储路径不能以'/'开头") String savePath,
-            @RequestParam @MultipartFileValid MultipartFile[] files) {
-        return Result.success(fileStorageService.uploadFiles(savePath, files));
-    }
-
-    @DeleteMapping("/delete")
-    @Operation(summary = "删除文件")
-    public Result<String> deleteFile(
-            @RequestParam(required = false, defaultValue = "")
-            @Pattern(regexp = "^(?!/).*$", message = "存储路径不能以'/'开头") String savePath,
-            @RequestParam String fileName) {
-        return Result.judge(fileStorageService.deleteFile(savePath, fileName));
-    }
+//    @PostMapping("/upload")
+//    @Operation(summary = "上传文件")
+//    public Result<String> uploadFile(
+//            @RequestParam(required = false, defaultValue = "")
+//            @Pattern(regexp = "^(?!/).*$", message = "存储路径不能以'/'开头") String savePath,
+//            @RequestParam @MultipartFileValid MultipartFile file) {
+//        return Result.success(fileStorageService.uploadFile(savePath, file));
+//    }
+//
+//    @PostMapping("/uploads")
+//    @Operation(summary = "上传多个文件")
+//    public Result<String[]> uploadFiles(
+//            @RequestParam(required = false, defaultValue = "")
+//            @Pattern(regexp = "^(?!/).*$", message = "存储路径不能以'/'开头") String savePath,
+//            @RequestParam @MultipartFileValid MultipartFile[] files) {
+//        return Result.success(fileStorageService.uploadFiles(savePath, files));
+//    }
+//
+//    @DeleteMapping("/delete")
+//    @Operation(summary = "删除文件")
+//    public Result<String> deleteFile(
+//            @RequestParam(required = false, defaultValue = "")
+//            @Pattern(regexp = "^(?!/).*$", message = "存储路径不能以'/'开头") String savePath,
+//            @RequestParam String fileName) {
+//        return Result.judge(fileStorageService.deleteFile(savePath, fileName));
+//    }
 
 }
