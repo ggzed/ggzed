4010d1d368eac02a6bade7c151a245ae51136f60
yifei <364398311@qq.com>
2024-09-06 12:14:21 +0800

    feat (echarts): ✨ 新增 ECharts 演示页面 ( 初步/未新增动态修改，等待二次更新 )

--- yf-vue-admin/src/views/demo/echarts/index.vue
+++ yf-vue-admin/src/views/demo/echarts/index.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-    模块快速开发中
+    <div ref="chartRef" style="width: 100%; height: 400px;"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
 // 数据
+import {ChartStrategy, useECharts} from "@/hooks/useECharts";
 
-// 方法
+// 引用容器的 ref
+const chartRef = ref<HTMLDivElement | null>(null);
 
-// 生命周期
+class LineOptionStrategy implements ChartStrategy {
+  // 配置项
+  getOptions() {
+    return {
+      xAxis: {
+        type: 'category',
+        data: ['', '', '', '', '', '', '']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: ["1", "1", "2", "1", "1", "1", "1"],
+          type: 'line'
+        }
+      ]
+    }
+  }
+}
+
+const {options} = useECharts(chartRef, new LineOptionStrategy().getOptions());
+
+setTimeout(() => {
+  options.series[0].data = [210, 200, 200, 200, 200, 220, 280];
+}, 2000)
+
 </script>
 
 <style lang="scss" scoped>
