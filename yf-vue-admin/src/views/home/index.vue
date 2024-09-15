0bf3492b906c6c94ba6464f6663afe670e69a589
yifei <364398311@qq.com>
2024-09-15 19:45:28 +0800

    chore(home): 🔨 首页架构修改

--- yf-vue-admin/src/views/home/index.vue
+++ yf-vue-admin/src/views/home/index.vue
@@ -1,40 +1,111 @@
 <template>
   <!--  页面  -->
   <div class="home-container">
-    <div>
-      <h3>漫游导航</h3>
-      <el-button type="primary" @click="systemStore.setTourStatus(true)">漫游导航开启</el-button>
-    </div>
+    <!--  欢迎信息  -->
+    <el-card>
+      <el-row justify="space-between">
+        <el-col :span="24">
+          <div class="user-info">
+            <el-avatar :size="80" :src="userStore.userInfo.avatar">
+              {{ userStore.userInfo.username }}
+            </el-avatar>
+            <div class="content">
+              <p class="single-line">您好 , {{ userStore.userInfo.nickname }} !</p>
+              <p class="double-line">今日天气晴朗，气温在15℃至25℃之间，东南风。今日天气晴朗，气温在15℃至25℃之间，东南风。</p>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </el-card>
 
-    <div>
-      <h3>设置</h3>
-      <el-button type="primary" @click="systemStore.settings.settingsVisible = true">打开设置</el-button>
-      <el-button type="success" @click="systemStore.settings.settingsVisible = true">打开设置</el-button>
-      <el-button type="warning" @click="systemStore.settings.settingsVisible = true">打开设置</el-button>
-      <el-button type="danger" @click="systemStore.settings.settingsVisible = true">打开设置</el-button>
-      <el-button type="info" @click="systemStore.settings.settingsVisible = true">打开设置</el-button>
-    </div>
+    <!--  小功能向导  -->
+    <el-row :gutter="20" class="action-cards">
+      <el-col :lg="6" :sm="12" :xs="24">
+        <el-card>
+          <div>
+            <svg-icon icon-class="hot-list" size="24"></svg-icon>
+            <el-text tag="b">每日热榜</el-text>
+          </div>
+          <div>
+            <el-icon>
+              <ArrowRightBold/>
+            </el-icon>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :lg="6" :sm="12" :xs="24">
+        <el-card>
+          <div>
+            <svg-icon icon-class="today-avatar" size="24"></svg-icon>
+            <el-text tag="b">照片推荐墙</el-text>
+          </div>
+          <div>
+            <el-icon>
+              <ArrowRightBold/>
+            </el-icon>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :lg="6" :sm="12" :xs="24">
+        <el-card @click="systemStore.settings.settingsVisible = true">
+          <div>
+            <svg-icon icon-class="page-personalize" size="24"></svg-icon>
+            <el-text tag="b">页面个性化</el-text>
+          </div>
+          <div>
+            <el-icon>
+              <ArrowRightBold/>
+            </el-icon>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :lg="6" :sm="12" :xs="24">
+        <el-card @click="systemStore.setTourStatus(true)">
+          <div>
+            <svg-icon icon-class="page-wizard" size="24"></svg-icon>
+            <el-text tag="b">页面向导</el-text>
+          </div>
+          <div>
+            <el-icon>
+              <ArrowRightBold/>
+            </el-icon>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
 
-    <div>
-      <h3>组件缓存测试</h3>
-      <div>count 值 : {{ count }}</div>
-      <br>
-      <el-button type="primary" @click="count += 1">count + 1</el-button>
-    </div>
+    <!--  PV , UV , Echarts   -->
+    <el-row :gutter="10" class="mt-5">
+      <el-col :span="16" :xs="24" style="margin-bottom: 10px;">
+        <!-- 访问趋势统计图 -->
+        <div ref="VisitTrend" class="visit-trend"></div>
+      </el-col>
+      <el-col :span="8" :xs="24" style="margin-bottom: 10px;">
+        <div class="image-wrapper">
+          <el-image
+              class="custom-image"
+              fit="cover"
+              loading="lazy"
+              src="https://api.vvhan.com/api/moyu"
+          ></el-image>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script lang="ts" setup>
 // 数据
 import {useSystemStore} from "@/store/modules/system";
-import {Ref} from "vue";
+import {useUserStore} from "@/store/modules/user";
 
 defineOptions({
   name: "Home",
   inheritAttrs: false,
 });
-const count: Ref<number> = ref<number>(0);
+
 const systemStore = useSystemStore();
+const userStore = useUserStore();
 </script>
 
 <style lang="scss" scoped>
@@ -44,4 +115,83 @@ const systemStore = useSystemStore();
   flex-direction: column; /* 设置基准线为上下方向 */
   justify-content: center;
 }
+
+.user-info {
+  display: flex;
+  gap: 20px;
+  align-items: center;
+
+  // 内部文字内容部分
+  .content {
+    flex: 1;
+    max-width: 80vw;
+
+    // 限制第一个p标签为单行
+    .single-line {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    // 限制第二个p标签为双行
+    .double-line {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: normal;
+    }
+  }
+}
+
+.action-cards {
+  margin-top: 10px;
+  margin-bottom: 20px;
+
+  .el-card {
+    margin-top: 10px;
+  }
+
+  :deep(.el-card__body) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+    cursor: pointer;
+
+    :first-child {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 8px;
+    }
+  }
+}
+
+// 摸鱼日历
+.image-wrapper {
+  width: 100%;
+  height: 93.4%;
+  overflow: hidden;
+  position: relative;
+}
+
+.custom-image {
+  width: 100%;
+  height: auto;
+  position: relative;
+  top: -6.6%;
+}
+
+//  U/PV 统计图
+.visit-trend {
+  height: 600px;
+  width: 100%;
+  background-color: red;
+}
+
 </style>
