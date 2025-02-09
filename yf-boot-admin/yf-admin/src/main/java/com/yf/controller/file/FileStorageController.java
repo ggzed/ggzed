269fdb129b802126673263ebf98e2c8b154f1385
yifei <364398311@qq.com>
2025-02-09 15:19:45 +0800

    feat ( file ): ✨ 新增统一文件上传接口

--- yf-boot-admin/yf-admin/src/main/java/com/yf/controller/file/FileStorageController.java
+++ yf-boot-admin/yf-admin/src/main/java/com/yf/controller/file/FileStorageController.java
@@ -1,11 +1,30 @@
 package com.yf.controller.file;
 
+import com.yf.constants.RedisKeyConstants;
+import com.yf.oss.constraints.MultipartFileValid;
 import com.yf.oss.storage.FileStorageService;
+import com.yf.rate_limiting.annotation.RateLimiter;
+import com.yf.rate_limiting.annotation.RateLimiters;
+import com.yf.rate_limiting.annotation.RateRule;
+import com.yf.rate_limiting.model.enums.LimitTypeEnum;
+import com.yf.result.Result;
+import com.yf.result.ResultCode;
+import com.yf.security.utils.SecurityUtil;
+import com.yf.utils.RedisUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 文件上传服务
@@ -20,16 +39,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FileStorageController {
 
-    private final FileStorageService fileStorageService;
+    /**
+     * key : save_path , value : 需要的权限
+     */
+    public static final Map<String, String> permissionCheckMap = Map.of(
+            "demo/markdown", "demo:markdown:list"
+    );
 
-//    @PostMapping("/upload")
-//    @Operation(summary = "上传文件")
-//    public Result<String> uploadFile(
-//            @RequestParam(required = false, defaultValue = "")
-//            @Pattern(regexp = "^(?!/).*$", message = "存储路径不能以'/'开头") String savePath,
-//            @RequestParam @MultipartFileValid MultipartFile file) {
-//        return Result.success(fileStorageService.uploadFile(savePath, file));
-//    }
+    private final FileStorageService fileStorageService;
+    private final RedisUtil redisUtil;
+
+    @PostMapping("/upload")
+    @Operation(summary = "上传文件")
+    @RateLimiters(rateLimiters = {
+            @RateLimiter(limitTypeEnum = LimitTypeEnum.IP,
+                    rateRules = {
+                            @RateRule,
+                            @RateRule(limit = 100, timeDuration = 1, timeUnit = TimeUnit.DAYS)
+                    }),
+    })
+    public Result<String> uploadFile(
+            @RequestParam(required = false, defaultValue = "")
+            @Pattern(regexp = "^(?!/).*$", message = "存储路径不能以'/'开头") String savePath,
+            @RequestParam @MultipartFileValid MultipartFile file) {
+        // 1. 校验权限
+        String permission = permissionCheckMap.get(savePath);
+        if (permission == null) {
+            return Result.failed(ResultCode.AUTH_ACCESS_UNAUTHORIZED);
+        } else {
+            Long userId = SecurityUtil.getUserId();
+            Set<String> permissionsCache = redisUtil.getCacheObject(RedisKeyConstants.USER_PERMISSIONS_CACHE_PREFIX + userId);
+            if (!permissionsCache.contains(permission)) {
+                return Result.failed(ResultCode.AUTH_ACCESS_UNAUTHORIZED);
+            }
+        }
+        // 2. 保存图片
+        return Result.success(fileStorageService.uploadFile(savePath, file));
+    }
 //
 //    @PostMapping("/uploads")
 //    @Operation(summary = "上传多个文件")
